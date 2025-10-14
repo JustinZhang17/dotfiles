f5e3670ad0b7d80d69282c0cdd5e52e43776126b
Justin Zhang <hi@justinjzhang.com>
2025-10-14 00:15:33 -0400

    docs: configure Neovim options for line numbers and scroll

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -1,6 +1,7 @@
 require "nvchad.options"
 
--- add yours here!
+local o = vim.o
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+o.relativenumber = true
+o.number = true
+o.scrolloff = 8
