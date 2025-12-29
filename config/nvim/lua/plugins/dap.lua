49999c94357466c3f767f3c227514b4e7b7cf6bd
Justin Zhang <hi@justinjzhang.com>
2025-12-28 19:50:19 -0500

    Add DAP configuration for debugging C, C++, and Rust

---
+++ config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,53 @@
+return {
+  -- Core DAP
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+      "theHamsta/nvim-dap-virtual-text",
+      "jay-babu/mason-nvim-dap.nvim",
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      local mason_dap = require("mason-nvim-dap")
+
+      mason_dap.setup({
+        ensure_installed = { "codelldb" },
+        handlers = {
+          function(config)
+            require('mason-nvim-dap').default_setup(config)
+          end,
+        },
+      })
+
+      -- DAP configs for each language
+      dap.configurations.cpp = {
+        {
+          name = "Launch file",
+          type = "codelldb", -- match the debugger installed via Mason
+          request = "launch",
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcmd() .. '/', 'file')
+          end,
+          cwd = '${workspaceFolder}',
+          stopOnEntry = false,
+          runInTerminal = true,
+        },
+      }
+
+      dap.configurations.c = dap.configurations.cpp
+      dap.configurations.rust = dap.configurations.cpp
+
+      dapui.setup()
+
+      -- Automatically open/close UI for debugging
+      dap.listeners.before.attach.dapui_config = function() dapui.open() end
+      dap.listeners.before.launch.dapui_config = function() dapui.open() end
+      dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
+      dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
+
+    end,
+  },
+}
